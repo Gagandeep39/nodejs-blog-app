6a38ad3d9174d66d39546469b618d3f194be5f17
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-10-19 01:34:53 +0530

    Created Posts component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
 import CreatePost from './components/CreatePost';
+import Posts from './components/Posts';
 
 function App() {
   return (
     <div className='container'>
       <h1 className='display-2'>Blog App</h1>
       <CreatePost />
+      <hr />
+      <h1 className='display-2'>Posts</h1>
+      <Posts />
     </div>
   );
 }
---
+++ client/src/components/Posts.js
@@ -0,0 +1,43 @@
+/**
+ * @author Gagandeep Singh
+ * @email singh.gagandeep3911@gmail.com
+ * @create date 2020-10-19 01:10:42
+ * @modify date 2020-10-19 01:10:42
+ * @desc Component to display a list of Posts
+ */
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+export default function Posts() {
+  const [posts, setPosts] = useState([]);
+  const url = process.env.REACT_APP_URL;
+  const postService = process.env.REACT_APP_POST_SERVICE;
+
+  const fetchPosts = async () => {
+    axios
+      .get(url + postService)
+      .then((response) => {
+        console.log(response.data);
+        setPosts(response.data);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  const renderedPosts = Object.values(posts).map((post) => (
+    <div
+      className='card'
+      key={post.id}
+      style={{ width: `30%`, marginBottom: `20px` }}
+    >
+      <div className='card-body'>
+        <h3> {post.title} </h3>
+      </div>
+    </div>
+  ));
+
+  return <div>{renderedPosts}</div>;
+}

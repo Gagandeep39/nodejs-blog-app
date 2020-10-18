98ad5363285c6cc24d8dcefbb67360dead29618a
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-10-19 01:08:14 +0530

    Fixed cors error

--- comment-service/index.js
+++ comment-service/index.js
@@ -8,11 +8,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const cors = require('cors');
 const app = express();
 const { randomBytes } = require('crypto');
 const commentsByPostId = {};
 
 app.use(bodyParser.json());
+app.use(cors());
 app.use(morgan('dev'));
 
 app.get('/posts/:id/comments', (req, res) =>
--- posts-service/index.js
+++ posts-service/index.js
@@ -8,11 +8,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const cors = require('cors');
 const app = express();
 const { randomBytes } = require('crypto');
 const posts = {};
 
 app.use(bodyParser.json());
+app.use(cors());
 app.use(morgan('dev'));
 
 app.get('/posts', (req, res) => res.send(posts));
